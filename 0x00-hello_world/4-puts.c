c87b92bfba143d552e88a7498b49788915bbe06a
Moses Adegoke <adegokemoses@yahoo.com>
2023-09-07 20:27:33 +0000

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints with put function
+ *
+ * Return: 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
