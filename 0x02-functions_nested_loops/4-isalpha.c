a71d002d059f6e3617650ff66be182a2dca1a408
Moses Adegoke <adegokemoses@yahoo.com>
2023-09-12 12:29:38 -0700

    4.isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,14 +5,12 @@
  *
  * @c: value to be checked
  *
- * Return: Returns 1 if c is a letter, 
- * lowercase or uppercase, Returns 0 otherwise
+ * Return: Returns 1 if c is a letter, Returns 0 otherwise
 */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 97) ||
-			(c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 		return (1);
 	return (0);
 }
