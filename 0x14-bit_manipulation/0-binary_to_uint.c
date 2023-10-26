b69be7c9c990fa16832905347bfcadb6e007c17c
Moses Adegoke <adegokemoses@yahoo.com>
2023-10-27 00:56:55 +0100

    bitwise

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function to convert binary string to unsigned int
+ * @b: pointer to a binary string
+ *
+ * Return: the converted unsigned integer, or 0 if there is one or more
+ * chars in the string b that is not 0 or 1 or if b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	/* Declare an integer variable i */
+	int i;
+	/* Declare an unsigned integer variable decimal and initialize it to 0 */
+	unsigned int decimal = 0;
+	/* Check if the input string b is NULL */
+	if (b == NULL)
+
+	/* If b is NULL, return 0 */
+	return (0);
+
+	/* Iterate through the binary string */
+	for (i = 0; b[i] != '\0'; ++i)
+	{
+		/**
+		 * Check if the character at index i in the
+		 * string b is not a valid binary digit
+		 */
+		if (b[i] != '0' && b[i] != '1')
+		/**
+		 * If the character at index i in the string b
+		 * is not a valid binary digit, return 0
+		 */
+		return (0);
+		/**
+		 * Convert the binary digit at index i in the
+		 * string b to decimal and update the result
+		 */
+		decimal = 2 * decimal + (b[i] - '0');
+		/* Return the converted unsigned integer */
+		return (decimal);
+	}
+}
