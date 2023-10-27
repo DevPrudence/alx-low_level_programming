4f32cc823003ae69c67dcc970641fb0da0638fe4
Moses Adegoke <adegokemoses@yahoo.com>
2023-10-27 02:38:07 +0100

    bit shift right

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,13 +11,12 @@
 /* Define the function get_bit */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    /* If the index is greater than 32, return -1 */
-    if (index > 32)
-        return (-1);
-
-    /**
+	/* If the index is greater than 32, return -1 */
+	if (index > 32)
+	return (-1);
+	/**
 	 * Right shift the bits of n by index places,
 	 * perform bitwise AND with 1, and return the result
 	 */
-    return ((n >> index) & 1);
+	return ((n >> index) & 1);
 }
